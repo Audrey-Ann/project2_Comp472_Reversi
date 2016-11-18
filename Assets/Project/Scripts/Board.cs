c02fc36105b050ad1e3b4c1460e2ab4995489852
Audrey <audreyjw@hotmail.com>
2016-11-18 10:22:59 -0500

    First commit for project2_COMP472

---
+++ Assets/Project/Scripts/Board.cs
@@ -0,0 +1,364 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour {
+
+	[SerializeField]
+	GameObject prefab;
+
+    public static Board board;
+
+    public Tile[,] squares = new Tile[8,8];
+
+    Dictionary<Tile, List<Tile>> tilesPlusNeighbours;
+
+    public static List<Board> listBoardStates = new List<Board>();
+
+    List<Tile> tileToFlip;
+
+    Tile.State turn;
+
+    void Start () {
+
+    tilesPlusNeighbours = new Dictionary<Tile, List<Tile>>();
+
+    tileToFlip = new List<Tile>();
+
+    turn = Tile.State.BLACK;
+
+    board = this;
+
+        for (int i = 0; i < 8; i++)
+        {
+			for (int j = 0; j < 8; j++)
+            {
+                squares[j, i] = Instantiate(prefab).GetComponent<Tile>();
+				squares [j, i].transform.parent = this.transform;
+                squares[j, i].x = j;
+                squares[j, i].y = i;
+			}
+		}
+
+        collectNeighbours(squares); 
+        Reset();
+
+    }
+
+    public void OnClickTile ( Tile tile )
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (tile.CurrentState == Tile.State.FREE)
+            {
+                tile.CurrentState = turn;
+
+                if (checkValidMove(tile, turn))
+                {
+                    listBoardStates.Add(board);
+                    switchTurn(); 
+                }
+                else
+                {
+                    tile.CurrentState = Tile.State.FREE;
+                }
+            }
+        }
+        FlipBoard();
+
+        // Debug.Log("click " + tile.x + ", " + tile.y);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Reset(); 
+        }
+	}
+
+    bool checkValidMove(Tile tile, Tile.State clickedColour)
+    {
+        List<Tile> tileNeighbours = new List<Tile>(); 
+            tileNeighbours = tilesPlusNeighbours[tile]; 
+        //Check for neighbours: 
+        foreach(Tile neighbour in tileNeighbours)
+        {   //Check if we can flip and check neighbours of the clicked tile:  
+            if (check(turn) && neighbour.CurrentState != Tile.State.FREE && neighbour.CurrentState != clickedColour){return true;}
+        }
+
+        return false; 
+    }
+
+    void FlipBoard()
+    {
+        if (tileToFlip.Count > 0)
+        {
+            foreach (Tile tile in tileToFlip)
+            {
+               // Debug.Log(squares[tile.y, tile.x].y + " " +  squares[tile.y, tile.x].x);
+
+                if (squares[tile.y, tile.x].CurrentState == Tile.State.WHITE)
+                {
+                    squares[tile.y, tile.x].CurrentState = Tile.State.BLACK;
+                }
+                else if (squares[tile.y, tile.x].CurrentState == Tile.State.BLACK)
+                {
+                    squares[tile.y, tile.x].CurrentState = Tile.State.WHITE;
+                }
+            }
+
+            tileToFlip.Clear();
+        }
+        else
+        {
+            Debug.Log("Nothing to be flipped"); 
+        }
+    }
+
+    private bool check(Tile.State turn)
+    {
+        for (int row = 0; row < 8; row++)
+        {
+            if (checkRow(row, turn))
+            {
+                return true;
+            }
+        }
+        for (int col = 0; col < 8; col++)
+        {
+            if (checkColumn(col, turn))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool checkRow(int row, Tile.State turnState)
+    {
+        bool valid = false;
+        int col = 0;
+        int playerColorTile = 0;
+        int opponentColorTile = 0;
+        bool nextToEachOther = false;
+
+        while (col < 8)
+        {
+            if (squares[col, row].CurrentState == turnState)
+            {
+                playerColorTile++;
+                //Make sure that the player's tiles are not next to each other: 
+                if (playerColorTile > 1 && squares[col, row].CurrentState == squares[col - 1, row].CurrentState)
+                {
+                    nextToEachOther = true;
+                }
+            }
+            if (playerColorTile > 0 && squares[col, row].CurrentState != turnState && squares[col, row].CurrentState != Tile.State.FREE)
+            {
+                //check if the tile before the oppenent one is not a free tile: 
+                if (squares[col - 1, row].CurrentState != Tile.State.FREE)
+                {
+                    tileToFlip.Add(squares[row, col]);
+                    opponentColorTile++;
+                }
+            }
+            col++;
+        }
+
+        //check constraints
+        if (playerColorTile >= 2 && opponentColorTile > 0 && nextToEachOther == false)
+        {
+            valid = true;
+        }
+
+        if(valid == false)
+        {
+            //purge the list of tiles to be flipped: 
+            tileToFlip.Clear();
+        }
+
+        return valid;
+    }
+   
+    private bool checkColumn(int col, Tile.State turnState)
+    {
+        bool valid = false;
+        int row = 0;
+        int playerColorTile = 0;
+        int opponentColorTile = 0;
+        bool nextToEachOther = false;
+
+        // count consecutive black tiles
+        while (row < 8)
+        {
+            if (squares[col, row].CurrentState == turnState)
+            {
+                playerColorTile++;
+
+                if (playerColorTile > 1 && squares[col, row].CurrentState == squares[col, row - 1].CurrentState)
+                {
+                    nextToEachOther = false;
+                }
+            }
+            if (playerColorTile > 0 && squares[col, row].CurrentState != turnState && squares[col, row].CurrentState != Tile.State.FREE)
+            {
+                //check if the tile before the oppenent one is not a free tile: 
+                if (squares[col, row - 1].CurrentState != Tile.State.FREE)
+                {
+                    tileToFlip.Add(squares[col, row]);
+                    opponentColorTile++;
+                }
+            }
+            row++;
+        }
+
+        // check constraint
+        if (playerColorTile >= 2 && opponentColorTile > 0 && nextToEachOther == false)
+        {
+            valid = true;
+        }
+
+        if (valid == false)
+        {
+            //purge the list of tiles to be flipped: 
+            tileToFlip.Clear();
+        }
+
+        return valid;
+    }
+
+    void switchTurn()
+    {
+        if (turn == Tile.State.BLACK)
+        {
+            turn = Tile.State.WHITE;
+        }
+        else if (turn == Tile.State.WHITE)
+        {
+            turn = Tile.State.BLACK;
+        }
+    }
+
+    void Reset()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                squares[j, i].CurrentState = Tile.State.FREE;
+            }
+        }
+
+        squares[3, 3].CurrentState = Tile.State.WHITE;
+        squares[4, 4].CurrentState = Tile.State.WHITE;
+        squares[3, 4].CurrentState = Tile.State.BLACK;
+        squares[4, 3].CurrentState = Tile.State.BLACK;
+    }
+
+    void collectNeighbours(Tile[,] list)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                List<Tile> neighbours = new List<Tile>();
+
+                //corners
+                if (i % 7 == 0 && j % 7 == 0)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        neighbours.Add(squares[j, i + 1]);
+                        neighbours.Add(squares[j + 1, i]);
+                        neighbours.Add(squares[j + 1, i + 1]);
+                    }
+                    else if (i == 7 && j == 7)
+                    {
+                        neighbours.Add(squares[j - 1, i - 1]);
+                        neighbours.Add(squares[j - 1, i]);
+                        neighbours.Add(squares[j, j - 1]);
+                    }
+                    else if (i == 0 && j == 7)
+                    {
+                        neighbours.Add(squares[j - 1, i]);
+                        neighbours.Add(squares[j - 1, i + 1]);
+                        neighbours.Add(squares[j, i + 1]);
+                    }
+                    else
+                    {
+                        neighbours.Add(squares[j, i - 1]);
+                        neighbours.Add(squares[j + 1, i - 1]);
+                        neighbours.Add(squares[j + 1, i]);
+                    }
+
+                }
+
+                //first row and column: 
+                else if (i > 0 && i < 7 && j == 7)
+                {
+                    neighbours.Add(squares[j - 1, i - 1]);
+                    neighbours.Add(squares[j - 1, i]);
+                    neighbours.Add(squares[j - 1, i + 1]);
+
+                    neighbours.Add(squares[j, i - 1]);
+                    neighbours.Add(squares[j, i + 1]);
+                }
+                //first row and column: 
+                else if (i > 0 && i < 7 && j == 0)
+                {
+
+                    neighbours.Add(squares[j, i - 1]);
+                    neighbours.Add(squares[j, i + 1]);
+
+                    neighbours.Add(squares[j + 1, i - 1]);
+                    neighbours.Add(squares[j + 1, i]);
+                    neighbours.Add(squares[j + 1, i + 1]);
+                }
+                //first row and column: 
+                else if (j > 0 && j < 7 && i == 0)
+                {
+                    neighbours.Add(squares[j - 1, i]);
+                    neighbours.Add(squares[j - 1, i + 1]);
+
+                    neighbours.Add(squares[j, i + 1]);
+
+                    neighbours.Add(squares[j + 1, i]);
+                    neighbours.Add(squares[j + 1, i + 1]);
+                }
+                //first and last row and column: 
+                else if (j > 0 && j < 7 && i == 7)
+                {
+                    neighbours.Add(squares[j - 1, i - 1]);
+                    neighbours.Add(squares[j - 1, i]);
+
+                    neighbours.Add(squares[j, i - 1]);
+
+                    neighbours.Add(squares[j + 1, i - 1]);
+                    neighbours.Add(squares[j + 1, i]);
+                }
+
+
+                //inside
+                else if (i != 0 && i != 7 && j != 0 && j != 7)
+                {
+                    neighbours.Add(squares[j - 1, i - 1]);
+                    neighbours.Add(squares[j - 1, i]);
+                    neighbours.Add(squares[j - 1, i + 1]);
+
+                    neighbours.Add(squares[j, i - 1]);
+                    neighbours.Add(squares[j, i + 1]);
+
+                    neighbours.Add(squares[j + 1, i - 1]);
+                    neighbours.Add(squares[j + 1, i]);
+                    neighbours.Add(squares[j + 1, i + 1]);
+                }
+
+                tilesPlusNeighbours.Add(squares[j, i], neighbours);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/Project/Scripts/Tile.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+
+    public enum State { FREE, WHITE, BLACK };
+
+    public int x;
+    public int y;
+
+    private State state;
+
+    public UnityEngine.UI.Image image;
+
+    public State CurrentState
+    {
+        get
+        {
+            return state;
+        }
+
+        set
+        {
+            switch (value)
+            {
+                case State.FREE:
+                    state = value;
+                    image.color = new Color(0, 0, 0, 0);
+                    break;
+                case State.WHITE:
+                    state = value;
+                    image.color = Color.white;
+                    break;
+                case State.BLACK:
+                    state = value;
+                    image.color = Color.black;
+                    break;
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Awake ()
+    {
+        image.color = new Color(0, 0, 0, 0);
+        state = State.FREE;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void setState(State s)
+    {
+        state = s;
+    }
+
+    public void OnClick ()
+    {
+        Board.board.OnClickTile(this);
+    }
+}
